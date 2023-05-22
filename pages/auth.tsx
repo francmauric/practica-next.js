6967eef61957a227e71d6640304609d76241be65
franc_mauric1 <franc_mauric1@hotmail.com>
2023-05-22 18:18:30 +0200

    empezando el register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Input from "@/components/Input"
 import {useCallback, useState} from 'react'
 
@@ -10,6 +11,18 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register',{
+                email,
+                name,
+                password
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }, [])
 
     return (
