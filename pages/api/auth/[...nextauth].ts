ff8b3438d5361d0f48776000958522667e084148
franc_mauric1 <franc_mauric1@hotmail.com>
2023-05-24 19:43:00 +0200

    instalacion del prismaadapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,19 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { compare } from 'bcrypt';
 import prismadb from '@/libs/prismadb';
 
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID  || '',
+      clientSecret: process.env.GITHUB_SECRET || ''
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID  || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
