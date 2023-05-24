1135b30c59264bbb868bd885259343976d76444c
franc_mauric1 <franc_mauric1@hotmail.com>
2023-05-24 18:12:48 +0200

    conexion del login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { compare } from 'bcrypt';
 import prismadb from '@/libs/prismadb';
 
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import Input from "@/components/Input"
 import {useCallback, useState} from 'react'
+import {signIn} from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub} from 'react-icons/fa'
 
 const Auth = () => {
+    const router = useRouter();
     const [email,setEmail] = useState('')
     const [name,setName] = useState('')
     const [password,setPassword] = useState('')
@@ -13,6 +19,24 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
 
+    
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            
+            router.push('/')
+        } catch (error) {
+            console.log(error)
+        }
+        
+    },[email, password, router])
+
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register',{
@@ -20,10 +44,13 @@ const Auth = () => {
                 name,
                 password
             });
+
+            login()
+
         } catch (error) {
             console.log(error);
         }
-    }, [email, name, password])
+    }, [email, name, password, login])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
@@ -60,9 +87,40 @@ const Auth = () => {
                                 type="password"
                                 value={password}
                             />
-                            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                                 {variant === 'login' ? 'login' : 'Sign up'}
                             </button>
+                            <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                                <div className='
+                                        w-10
+                                        h-10
+                                        bg-white
+                                        rounded-full
+                                        flex
+                                        items-center
+                                        justify-center
+                                        cursor-pointer
+                                        hover:opacity-80
+                                        transition
+                                '>
+                                    <FcGoogle size={30}/>
+                                </div>
+                                <div className='
+                                        w-10
+                                        h-10
+                                        bg-white
+                                        rounded-full
+                                        flex
+                                        items-center
+                                        justify-center
+                                        cursor-pointer
+                                        hover:opacity-80
+                                        transition
+                                '>
+                                    <FaGithub size={30}/>
+                                </div>
+                            </div>
+
                             <p className="text-neutral-500 mt-12">
                                    {variant === 'login' ? 'First time using Netfilx' : 'Already have an account?'}
                                 <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursos-pointer">
