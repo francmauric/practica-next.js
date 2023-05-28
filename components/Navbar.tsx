6e485967325de0eb8b7ac2a9c1f984ed67941912
franc_mauric1 <franc_mauric1@hotmail.com>
2023-05-28 23:28:27 +0200

    continuando con el componente navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -64,7 +64,7 @@ const toggleAccountMenu = useCallback(() => {
                   <div className='w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden'>
                     <img src="/images/default-green.png" alt="" />
                   </div>
-                  <BsChevronDown className='text-white transition'/>
+                  <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`}/>
                   <AccountMenu visible={showAccountMenu} />
                 </div>
              </div>
